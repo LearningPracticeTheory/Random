d7a3875c00f67c3c7c03d2aad7397bb17648bd49
TinyA <1097912818@qq.com>
2018-09-22 13:45:47 +0800

    The input data error handing

--- src/RandomNumberGenerator.java
+++ src/RandomNumberGenerator.java
@@ -1,6 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,6 +12,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
@@ -27,31 +30,48 @@ public class RandomNumberGenerator extends JFrame {
 	JPanel panelEast = new JPanel();
 	JPanel panelEastUp = new JPanel();
 	JPanel panelEastCenter = new JPanel();
-	JButton random = new JButton("random");
+	JButton jbRandom = new JButton("random");
 //	JButton mark = new JButton("mark");
-	JButton log = new JButton("log");
-	JButton clear = new JButton("clear");
+	JButton jbLog = new JButton("log");
+	JButton jbClear = new JButton("clear");
 	JTextArea jtaCenter = new JTextArea();
 	List<JCheckBox> boxesList = new LinkedList<>();
+	
+//	private static MyDialog dialog = null;
 
 	public static void main(String args[]) {
 		new RandomNumberGenerator().launchFrame();
 	}
 	
 	public void launchFrame() {
+		
+//		Integer i = Integer.parseInt("A"); //NumberFormatException
+		
+		new MyDialog(this, "Enter", true); //true -> JFrame will display after this dialog
+		
+		//No input course or number || Close dialog directly || Cancel || praseInt(String) != Integer
+		if(course == null || course.length() == 0 || 
+				number == null || number.length() == 0 ||
+				integerNum == null) {
+//System.out.println("NULL");
+			System.exit(0); //KIA
+//			return; //not just return
+		}
+		
 		this.setSize(WIDTH, HEIGHT);
 		this.setTitle(title);
 		this.getContentPane().setBackground(color);
 		this.setLayout(new BorderLayout());
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		new MyDialog(this, "Enter", true);
 		southPanel();
 		centerPanel();
 		eastPanel();
 		this.add(panelEast, BorderLayout.EAST);
 		this.add(panelSouth, BorderLayout.SOUTH);
+		
 		this.setVisible(true);
+		
 	}
 	
 	private void eastPanel() {
@@ -81,13 +101,19 @@ public class RandomNumberGenerator extends JFrame {
 	
 	private void southPanel() {
 		panelSouth.setLayout(new GridLayout(1, 3));
-		panelSouth.add(random);
+		panelSouth.add(jbRandom);
 //		panelSouth.add(mark);
-		panelSouth.add(log);
-		panelSouth.add(clear);
+		panelSouth.add(jbLog);
+		panelSouth.add(jbClear);
 	}
 	
-	private class MyDialog extends JDialog {
+	String course = null;
+	String number = null;
+	Integer integerNum = null;
+	
+	private class MyDialog extends JDialog 
+	implements ActionListener{ //just implements on JFrame, will throw NullPointerException
+							//when click confirm to send message to ActionEvent
 
 		private static final long serialVersionUID = 1L;
 		JButton jbConfirm = new JButton("Confirm");
@@ -101,16 +127,21 @@ public class RandomNumberGenerator extends JFrame {
 		JPanel jpCenterRight = new JPanel();
 		JPanel jpSouth = new JPanel();
 		
-		MyDialog(JFrame frame, String title, boolean modal) {
+		MyDialog(RandomNumberGenerator frame, String title, boolean modal) {
 			super(frame, title, modal);
 			this.setSize(200, 150);
 			this.setLocationRelativeTo(null);
 			this.setDefaultCloseOperation(MyDialog.DISPOSE_ON_CLOSE);
+			
 			centerPanel();
 			southPanel();
 			this.setLayout(new BorderLayout());
 			this.add(jpCenter, BorderLayout.CENTER);
 			this.add(jpSouth, BorderLayout.SOUTH);
+			
+			jbConfirm.addActionListener(this);
+			jbCancel.addActionListener(this);
+			
 			this.setVisible(true);
 		}
 		
@@ -133,6 +164,51 @@ public class RandomNumberGenerator extends JFrame {
 			jpSouth.add(jbCancel);
 		}
 		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource().equals(jbConfirm)) {
+				
+				confirmPerformed();
+				
+			} else if(e.getSource().equals(jbCancel)) {
+				course = null;
+				number = null;
+				integerNum = null;
+				this.dispose();
+			}
+		}
+		
+		private void confirmPerformed() {
+			course = jtfCourse.getText().trim();
+			number = jtfNumber.getText().trim();
+			
+			if(course == null || course.length() == 0) { //NOT while
+				JOptionPane.showMessageDialog(this, "Course can't be NULL!", 
+						"Error", JOptionPane.INFORMATION_MESSAGE);
+				jtfCourse.requestFocus(true);
+			} else if(number == null || number.length() == 0) { //NOT while
+				JOptionPane.showMessageDialog(this, "Number can't be NULL!", 
+						"Error", JOptionPane.INFORMATION_MESSAGE);
+				jtfNumber.requestFocus(true);
+			} else if(number.length() > 0) {
+				try {
+					integerNum = Integer.parseInt(number);
+//System.out.println(intgerNum);
+				} catch(NumberFormatException exception) {
+					integerNum = null;
+					JOptionPane.showMessageDialog(this, "Please input an integer number!", 
+							"Error", JOptionPane.INFORMATION_MESSAGE);
+					jtfNumber.setText("");
+					jtfNumber.requestFocus(true);
+				}
+				if(integerNum != null) {
+					this.dispose();
+				}
+			} else {
+				this.dispose();
+			}
+		}
+		
 	}
-	
+
 }
